138707f40b8df28a5ef04637c550ffdbb4422aad
Johan Valdivia <crisvaldivia1093@gmail.com>
2022-11-07 19:38:14 -0500

    Update
    Certus Entrega Protecto UD1

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function datos() {
+  document.getElementById("nombre").innerHTML = "Cristhian Johan";
+  document.getElementById("apellidos").innerHTML = "Valdivia Salas";
+  document.getElementById("celular").innerHTML = "988525501";
+  document.getElementById("email").innerHTML = "crisvaldivia1093@gmail.com";
+}
